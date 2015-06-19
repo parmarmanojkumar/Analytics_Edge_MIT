a7c3873ebeb860872a04b11b82d3ed2cbb93bd83
Manojkumar Parmar <parmarmanojkumar@gmail.com>
2015-06-19 18:26:46 +0200

    Unit 2: Ungraded assignment statedata is over elantrasale is pending

--- 02_Unit2/03_Assignment/state_assignment.R
+++ 02_Unit2/03_Assignment/state_assignment.R
@@ -11,3 +11,25 @@ LEM = lm(Life.Exp ~ Population + Income + Illiteracy + Murder + HS.Grad +
            Frost + Area, data = statedata)
 summary(LEM)
 plot(statedata$Income, statedata$Life.Exp)
+LEM1 = lm(Life.Exp ~ Income + Murder + HS.Grad + 
+                 Frost, data = statedata)
+summary(LEM1)
+LEM2 = lm(Life.Exp ~ Illiteracy + Murder + HS.Grad + 
+                  Frost, data = statedata)
+summary(LEM2)
+LEM3 = lm(Life.Exp ~ Population + Murder + HS.Grad + 
+                  Frost, data = statedata)
+summary(LEM3)
+
+predictLE = predict(LEM3)
+statedata$predictLE = predictLE
+statedata$state.name[which.min(statedata$predictLE)]
+statedata$state.name[which.min(statedata$Life.Exp)]
+
+statedata$state.name[which.max(statedata$predictLE)]
+statedata$state.name[which.max(statedata$Life.Exp)]
+statedata$lediff = abs(statedata$predictLE - statedata$Life.Exp)
+
+statedata$state.name[which.min(statedata$lediff)]
+statedata$state.name[which.max(statedata$lediff)]
+
