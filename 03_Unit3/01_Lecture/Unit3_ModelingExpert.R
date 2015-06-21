adf5aad0c879b9b26c47078ae16168b584a44d4e
Manojkumar Parmar <parmarmanojkumar@gmail.com>
2015-06-21 21:25:11 +0200

    Unit 3: experts analylis lecture over

--- 03_Unit3/01_Lecture/Unit3_ModelingExpert.R
+++ 03_Unit3/01_Lecture/Unit3_ModelingExpert.R
@@ -39,7 +39,10 @@ predictTrain = predict(QualityLog, type="response")
 summary(predictTrain)
 tapply(predictTrain, qualityTrain$PoorCare, mean)
 
-
+#Quiz
+QualityLogq = glm(PoorCare ~ StartedOnCombination + ProviderCount, 
+                  data=qualityTrain, family=binomial)
+summary(QualityLogq)
 
 # Video 5
 
@@ -85,4 +88,25 @@ plot(ROCRperf)
 plot(ROCRperf, colorize=TRUE)
 
 # Add threshold labels 
-plot(ROCRperf, colorize=TRUE, print.cutoffs.at=seq(0,1,by=0.1), text.adj=c(-0.2,1.7))
+plot(ROCRperf, colorize=TRUE, print.cutoffs.at=seq(0,1,by=0.2), text.adj=c(-0.2,1.7))
+
+#Prediction
+predictTest = predict(QualityLog, type = "response", newdata = qualityTest)
+t = 0.2
+a = table(qualityTest$PoorCare, predictTest > t)
+accuracy = (a[1] + a[4])/ sum(a)
+error.rate = (a[2] + a[3]) / sum(a)
+sesnitivity = a[4]/(a[2] + a[4])
+specificity = a[1]/(a[1] + a[3])
+false.neg.error.rate = a[2]/ (a[2] + a[4])
+false.pos.error.rate = a[3]/(a[1] + a[3])
+
+t
+a
+accuracy 
+error.rate 
+sesnitivity
+specificity
+false.neg.error.rate
+false.pos.error.rate
+
