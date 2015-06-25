bf1878324fd29f5326f7dcd42c1e76b54075886d
Manojkumar Parmar <parmarmanojkumar@gmail.com>
2015-06-25 23:39:28 +0200

    Unit 3: Parole assignment is over

--- 03_Unit3/03_Assignment/parole_assignment.R
+++ 03_Unit3/03_Assignment/parole_assignment.R
@@ -20,3 +20,36 @@ test = subset(parole, split == FALSE)
 parolelm = glm(violator ~ ., data = train, family= "binomial")
 summary(parolelm)
 exp(1.61)
+
+testquiz = train[1,]
+testquiz$male =1
+testquiz$race = 1
+testquiz$state = levels(testquiz$state)[1]
+testquiz$age = 50
+testquiz$time.served = 3
+testquiz$max.sentence = 12
+testquiz$multiple.offenses = 0
+testquiz$crime = levels(testquiz$crime)[2]
+predictq = predict(parolelm, type = "response", newdata = testquiz)
+b = - log((1/predictq) - 1)
+odds = exp(b)
+
+#problem5
+pred.violator = predict(parolelm, type = "response", newdata = test)
+max(pred.violator)
+t = 0.2
+a = table(test$violator, pred.violator > t)
+accuracy = (a[1] + a[4])/ sum(a)
+error.rate = (a[2] + a[3]) / sum(a)
+sesnitivity = a[4]/(a[2] + a[4])
+specificity = a[1]/(a[1] + a[3])
+false.neg.error.rate = a[2]/ (a[2] + a[4])
+false.pos.error.rate = a[3]/(a[1] + a[3])
+
+basemodel = 1 - (sum(test$violator) / nrow(test))
+
+#ROCR
+library(ROCR)
+ROCRpredTest = prediction(pred.violator, test$violator)
+auc = as.numeric(performance(ROCRpredTest,"auc")@y.values)
+auc
